0801b46571dcf197fe3b84e40b8bdf6d22c753ca
Jaturawit Sooksompong <jaturawit@verismart.co.th>
2020-09-09 12:09:42 +0700

    fix folder

--- MoongImport/src/MoongImport.java
+++ MoongImport/src/MoongImport.java
@@ -9,8 +9,8 @@ import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 public class MoongImport {
 
-    // Connect to your database.
-    public static void main(String[] args) {
+	// Connect to your database.
+	public static void main(String[] args) {
     	
 		SQLServerDataSource ds = new SQLServerDataSource();
 		ds.setUser("sa");
@@ -49,51 +49,11 @@ public class MoongImport {
             	SQL = "SELECT * INTO dbo." + file[i] + 
             			" FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0','Excel 12.0; Database=E:\\FTP\\" + file[i] + ".xlsx', [Sheet1$]);";
                 stmt.execute(SQL);
-                
+          
+                		
                 System.out.println(file[i] + ".xlsx - done");
            
             } 
-        	
-//        	
-//        	// NetsalesGTbyCustomerbySKU.xlsx
-//        	SQL = "IF OBJECT_ID('dbo.NetsalesGTbyCustomerbySKU', 'U') IS NOT NULL Drop Table dbo.NetsalesGTbyCustomerbySKU";
-//        	stmt.execute(SQL);
-//        	
-//        	SQL = "SELECT * INTO dbo.NetsalesGTbyCustomerbySKU " + 
-//        			"FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0','Excel 12.0; Database=E:\\FTP\\NetsalesGTbyCustomerbySKU.xlsx', [Sheet1$]);";
-//            stmt.execute(SQL);
-//            
-//            System.out.println("NetsalesGTbyCustomerbySKU.xlsx - done");
-//            
-//            // DummyHistoryNetsales.xlsx
-//        	SQL = "IF OBJECT_ID('dbo.DummyHistoryNetsales', 'U') IS NOT NULL Drop Table dbo.DummyHistoryNetsales";
-//        	stmt.execute(SQL);
-//        	
-//        	SQL = "SELECT * INTO dbo.DummyHistoryNetsales " + 
-//        			"FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0','Excel 12.0; Database=E:\\FTP\\DummyHistoryNetsales.xlsx', [Sheet1$]);"; 
-//            stmt.execute(SQL);
-//            
-//            System.out.println("DummyHistoryNetsales.xlsx - done");
-//            
-//            // CustomerMaster.xlsx        
-//            SQL = "IF OBJECT_ID('dbo.CustomerMaster', 'U') IS NOT NULL Drop Table dbo.CustomerMaster";
-//        	stmt.execute(SQL);
-//        	
-//        	SQL = "SELECT * INTO dbo.CustomerMaster " + 
-//        			"FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0','Excel 12.0; Database=E:\\FTP\\CustomerMaster.xlsx', [Sheet1$]);"; 
-//            stmt.execute(SQL);
-//            
-//            System.out.println("CustomerMaster.xlsx - done");
-//            
-//            // Target.xlsx
-//            SQL = "IF OBJECT_ID('dbo.Target', 'U') IS NOT NULL Drop Table dbo.Target";
-//        	stmt.execute(SQL);
-//        	
-//        	SQL = "SELECT * INTO dbo.Target " + 
-//        			"FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0','Excel 12.0; Database=E:\\FTP\\Target.xlsx', [Sheet1$]);"; 
-//            stmt.execute(SQL);
-//            
-//            System.out.println("Target.xlsx - done");
         }
         // Handle any errors that may have occurred.
         catch (SQLException e) {
